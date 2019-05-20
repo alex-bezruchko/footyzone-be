82d9a1bdfddb1d9ffff165b7c7cfe7e138ab3557
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-05-20 17:20:22 -0400

    make insert fx async

--- data/helpers/postDb.js
+++ data/helpers/postDb.js
@@ -1,4 +1,4 @@
-const db = require('../dbConfig.js');
+const db = require("../dbConfig.js");
 
 module.exports = {
   welcomePosts,
@@ -10,51 +10,52 @@ module.exports = {
   update,
   remove,
   removeByUser,
-  fetchUsersPosts
+  fetchUsersPosts,
 };
 
 function welcomePosts() {
-  return db('posts').limit(5).orderBy('id');
+  return db("posts")
+    .limit(5)
+    .orderBy("id");
 }
 
 function latest() {
-  return db('posts').limit(10);
+  return db("posts").limit(10);
 }
 
 function fetchAll() {
-  return db('posts')
+  return db("posts");
 }
 
 function fetchUsersPosts(id) {
-  return db('posts').where({'user_id': id})
+  return db("posts").where({ user_id: id });
 }
 
 function fetchAllCategories() {
-  return db('categories')
+  return db("categories");
 }
 
 function getByCategoryId(id) {
-  return db('posts')
-    .where({ category_id: id })
-    // .then()
+  return db("posts").where({ category_id: id });
+  // .then()
 }
 
 function getById(id) {
-  return db('posts')
+  return db("posts")
     .where({ id })
     .first();
 }
 
-function insert(post) {
-  return db('posts')
-    .insert(post, 'id')
+async function insert(post) {
+  return db("posts")
+    .insert(post, "id")
     .then(ids => {
       return getById(ids[0]);
     });
 }
 
 async function update(id, changes) {
-  return db('posts')
+  return db("posts")
     .where({ id })
     .update(changes)
     .then(function() {
@@ -63,14 +64,13 @@ async function update(id, changes) {
 }
 
 function remove(id) {
-  return db('posts')
-    .where('id', id)
+  return db("posts")
+    .where("id", id)
     .del();
 }
 
 function removeByUser(user_id) {
-  return db('posts')
-    .where('user_id', user_id)
+  return db("posts")
+    .where("user_id", user_id)
     .del();
 }
-
