6a9188e7d02ddb9d98eaab5255096b9f52c65762
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-07-29 09:30:22 -0400

    add newslikes table

---
+++ data/migrations/20190729092940_newslikes.js
@@ -0,0 +1,20 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("newslikes", function(newslikes) {
+    newslikes.increments();
+    newslikes
+      .integer("user_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("users");
+    newslikes
+      .integer("news_id")
+      .unsigned()
+      .references("id")
+      .inTable("news");
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("newslikes");
+};
