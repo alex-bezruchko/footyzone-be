4b8371e20bda8b18f0133edb7c133a3662ac9e50
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-07-31 16:20:08 -0400

    add avatar when user logs in

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -1,51 +1,57 @@
-const router = require('express').Router();
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const users = require('./../data/helpers/userDb');
-const secret = require('./../config/secrets').jwtSecret;
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const users = require("./../data/helpers/userDb");
+const secret = require("./../config/secrets").jwtSecret;
 
-router.post('/register', (req, res) => {
-    let user = req.body;
-    const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
-    user.password = hash;
-    users.insert(user)
-      .then(saved => {
-        res.status(201).json(saved);
-      })
-      .catch(error => {
-        res.status(500).json(error);
-      });
-  });
-  
-  router.post('/login', (req, res) => {
-    let { username, password } = req.body;
-    users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          const token = generateToken(user);
-          res.status(200).json({
-            token, message: `Welcome ${user.username}!`, username: user.username, user_id: user.id
-          });
-        } else {
-          res.status(401).json({ message: 'Invalid Credentials' });
-        }
-      })
-      .catch(error => {
-        console.log(error)
-        res.status(500).json(error);
-      });
-  });
-  
-  function generateToken(user) {
-    const payload = {
-      subject: user.id,
-      username: user.username
-    }
-    const options = {
-      expiresIn: '1d',
-    }
-    return jwt.sign(payload, secret, options)
-  }
-  
-  module.exports = router;
+router.post("/register", (req, res) => {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
+  user.password = hash;
+  users
+    .insert(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.post("/login", (req, res) => {
+  let { username, password } = req.body;
+  users
+    .findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({
+          token,
+          message: `Welcome ${user.username}!`,
+          username: user.username,
+          user_id: user.id,
+          avatar: user.avatar,
+        });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json(error);
+    });
+});
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+  };
+  const options = {
+    expiresIn: "1d",
+  };
+  return jwt.sign(payload, secret, options);
+}
+
+module.exports = router;
