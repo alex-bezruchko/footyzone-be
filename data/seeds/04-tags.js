287707599df7de1c1eb780bc78996ed850a7928b
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-08-06 16:40:29 -0400

    remove subtags (table, helpers, seeeds too

--- data/seeds/04-tags.js
+++ data/seeds/04-tags.js
@@ -1,16 +1,53 @@
 exports.seed = function(knex, Promise) {
   return knex("tags").insert([
     {
-      tag_name: "Teams",
-      tag_slug: "teams",
+      tag_name: "Leo Messi",
+      tag_slug: "messi",
     },
     {
-      tag_name: "Players",
-      tag_slug: "players",
+      tag_name: "Ronaldo",
+      tag_slug: "ronaldo",
     },
     {
-      tag_name: "Transfers",
-      tag_slug: "transfers",
+      tag_name: "Manchester United",
+      tag_slug: "manutd",
+    },
+    {
+      tag_name: "Real Madrid",
+      tag_slug: "real-madrid",
+    },
+    {
+      tag_name: "Liverpool",
+      tag_slug: "liverpool",
+    },
+    {
+      tag_name: "Dembele",
+      tag_slug: "dembele",
+    },
+
+    {
+      tag_name: "Barcelona",
+      tag_slug: "barcelona",
+    },
+    {
+      tag_name: "Neymar Jr",
+      tag_slug: "neymar",
+    },
+    {
+      tag_name: "Raheem Sterling",
+      tag_slug: "raheem-sterling",
+    },
+    {
+      tag_name: "PSG",
+      tag_slug: "psg",
+    },
+    {
+      tag_name: "Bayern Munich",
+      tag_slug: "bayern-munich",
+    },
+    {
+      tag_name: "Gareth Bale",
+      tag_slug: "gareth-bale",
     },
   ]);
 };
