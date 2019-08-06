818e1fbdaae97e4380ca4245047af9c17a15f2e6
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-08-05 22:46:24 -0400

    fix db helper

--- news/newsRoutes.js
+++ news/newsRoutes.js
@@ -289,6 +289,7 @@ router.post("/", restricted, async (req, res) => {
             .status(201)
             .json({ addedNews, message: "News was successfully added." });
         } else {
+          console.log(addedNews);
           res.status(404).json({ message: "Theere was an error adding it." });
         }
       }
