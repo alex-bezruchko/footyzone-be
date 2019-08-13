e2942202d00b8ea21ec2ad1e62c09a094eb7d56f
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-08-13 13:08:15 -0400

    fix post login to async fx

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const users = require("./../data/helpers/userDb");
+const Users = require("./../data/helpers/userDb");
 const secret = require("./../config/secrets").jwtSecret;
 
 router.post("/register", (req, res) => {
@@ -12,7 +12,7 @@ router.post("/register", (req, res) => {
     user.avatar =
       "https://res.cloudinary.com/htg1iqq1p/image/upload/v1564598526/fwwckvx64nj7tjzxiyne.png";
   }
-  users
+  Users
     .insert(user)
     .then(saved => {
       console.log(saved);
@@ -31,30 +31,28 @@ router.post("/register", (req, res) => {
     });
 });
 
-router.post("/login", (req, res) => {
+router.post("/login", async (req, res) => {
   let { username, password } = req.body;
   let lowerCased = username.toLowerCase();
-  users
-    .findBy({ lowerCased })
-    .first()
-    .then(user => {
-      if (user && bcrypt.compareSync(password, user.password)) {
-        const token = generateToken(user);
-        res.status(200).json({
-          token,
-          message: `Welcome ${user.username}!`,
-          username: user.username,
-          user_id: user.id,
-          avatar: user.avatar,
-        });
-      } else {
-        res.status(401).json({ message: "Invalid Credentials" });
-      }
-    })
-    .catch(error => {
-      console.log(error);
-      res.status(500).json(error);
-    });
+
+  try {
+    let foundUser = await Users.findBy(lowerCased);
+    if (foundUser && bcrypt.compareSync(password, foundUser.password)) {
+      const token = generateToken(foundUser);
+      res.status(200).json({
+        token,
+        message: `Welcome ${foundUser.username}!`,
+        username: foundUser.username,
+        user_id: foundUser.id,
+        avatar: foundUser.avatar,
+      });
+    } else {
+      res.status(401).json({ message: "Invalid Credentials" });
+    }
+  } catch (e) {
+    console.log(error);
+    res.status(500).json(error);
+  }
 });
 
 function generateToken(user) {
