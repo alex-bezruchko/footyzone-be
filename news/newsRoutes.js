e3337881cdd7b64c8c948b4d5f26fc0e353bf3fe
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-08-28 09:56:07 -0400

    fix edgecase in the inserLikes route

--- news/newsRoutes.js
+++ news/newsRoutes.js
@@ -225,7 +225,7 @@ router.post("/newslikes", async (req, res) => {
     const likes = await newsDb.fetchAllLikes();
     if (likes) {
       let duplicate = likes.filter(like => (like.news_id === newLike.news_id && like.user_id === newLike.user_id))
-      if (duplicate || duplicate.length === 0) {
+      if (duplicate || duplicate.length > 0) {
         // let newLike =
         res.status(400).json({ message: "Message duplicate" })
       } else {
